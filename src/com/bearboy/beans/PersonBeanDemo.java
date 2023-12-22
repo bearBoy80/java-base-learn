677607ebb17b3a93d2083e83d26c15cf21a6d536
bearboy80 <2373319771@qq.com>
2023-12-22 15:29:37 +0800

    add javabeans demo

--- src/com/bearboy/beans/PersonBeanDemo.java
+++ src/com/bearboy/beans/PersonBeanDemo.java
@@ -1,9 +1,6 @@
 package com.bearboy.beans;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyVetoException;
+import java.beans.*;
 import java.util.Arrays;
 
 public class PersonBeanDemo {
@@ -14,16 +11,8 @@ public class PersonBeanDemo {
                 .forEach(x-> System.out.println("Person 类对应的属性：" + x.getName()));
         Arrays.stream(beanInfo.getMethodDescriptors())
                 .forEach(x-> System.out.println("Person 类对应的方法：" +x.getName()));
-        person.addPropertyChangeListener(event->{
-            // 属性变化通知事件
-            System.out.printf("监听到属性[%s] 内容变化（事件来源：%s），老值：%s，新值：%s\n",
-                    event.getPropertyName(),
-                    event.getSource(),
-                    event.getOldValue(),
-                    event.getNewValue()
-            );
-        });
-        person.addVetoableChangeListener(event -> {
+        person.addVetoableChangeListener(PersonBeanDemo::VetoableChange);
+        person.addPropertyChangeListener(event -> {
             System.out.printf("Vetoable监听到属性[%s] 内容变化（事件来源：%s），老值：%s，新值：%s\n",
                     event.getPropertyName(),
                     event.getSource(),
@@ -32,5 +21,23 @@ public class PersonBeanDemo {
             );
         });
         person.setName("jacktao");
+        //无法设置成功
+        person.setName("admin");
+    }
+
+    private static void VetoableChange(PropertyChangeEvent event) throws PropertyVetoException {
+        if ("name".equals(event.getPropertyName())) {
+            String newName = (String) event.getNewValue();
+            if (newName.equals("admin")) {
+                throw new PropertyVetoException("Name cannot be 'admin'", event);
+            }
+        }
+        // 属性变化通知事件
+        System.out.printf("监听到属性[%s] 内容变化（事件来源：%s），老值：%s，新值：%s\n",
+                event.getPropertyName(),
+                event.getSource(),
+                event.getOldValue(),
+                event.getNewValue()
+        );
     }
 }
