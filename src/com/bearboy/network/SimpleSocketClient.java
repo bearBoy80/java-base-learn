868fefded70b341bc3cee229266b4160bc61bae1
bearboy80 <2373319771@qq.com>
2023-12-31 20:19:20 +0800

    java socket demo

--- src/com/bearboy/network/SimpleSocketClient.java
+++ src/com/bearboy/network/SimpleSocketClient.java
@@ -2,14 +2,34 @@ package com.bearboy.network;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SimpleSocketClient {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost",8080);
+            Scanner scanner = new Scanner(System.in);
+            Socket socket = new Socket("localhost", 8080);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            //读取服务器端数据
-            System.out.println(reader.readLine());
+            while (true) {
+                String message = reader.readLine();
+                System.out.println("接收到server 发送的消息：" + message);
+                if ("bye".equals(message)) {
+                    //接收到server端发送的bye后，关闭程序
+                    break;
+                }
+                System.out.println("请输入数据要发送给server的数据:");
+                String str = scanner.nextLine() + "\n";
+                socket.getOutputStream().write(str.getBytes());
+                socket.getOutputStream().flush();
+                //如果控制台输入bye 退出程序
+                if (str.contains("bye")){
+                    break;
+                }
+            }
+            socket.close();
+            reader.close();
+            scanner.close();
+            System.out.println("client 关闭成功");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/com/bearboy/network/SimpleSocketServer.java
+++ src/com/bearboy/network/SimpleSocketServer.java
@@ -1,9 +1,12 @@
 package com.bearboy.network;
 
-import java.io.IOException;
+import sun.nio.ch.sctp.SctpNet;
+
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SimpleSocketServer {
     public static void main(String[] args) {
@@ -12,9 +15,33 @@ public class SimpleSocketServer {
             ServerSocket serverSocket = new ServerSocket();
             serverSocket.bind(address);
             Socket socket = serverSocket.accept();
-            String message ="服务端收到client连接请求";
-            socket.getOutputStream().write(message.getBytes());
+            System.out.println("接收到客户端连接请求");
+            String message ="服务端收到client连接请求\n";
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(message.getBytes());
+            outputStream.flush();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Scanner console = new Scanner(System.in);
+            while (true){
+                message = reader.readLine();
+                if ("bye".equals(message)){
+                    break;
+                }else {
+                    System.out.println("接收到客户端的message数据:" + message);
+                }
+                System.out.println("请输入数据要发送给client的数据:");
+                String input= console.nextLine() + "\n";
+                outputStream.write(input.getBytes());
+                outputStream.flush();
+                //如果控制台输入bye 退出程序
+                if (input.contains("bye")){
+                    break;
+                }
+            }
+            outputStream.close();
+            console.close();
             socket.close();
+            System.out.println("server关闭成功");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
